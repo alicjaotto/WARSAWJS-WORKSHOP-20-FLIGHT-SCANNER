50cd6e830f56a506039c92fee0bbfa0626dfbac6
ala <alicja.otto@gmail.com>
2019-02-13 15:53:13 +0100

    Updates

---
+++ precache-manifest.7daa46e6c5b249462eb98d957c85258d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "32b5f3ff1e31b5b6e28c",
+    "url": "/warsawjs-workshop-20-flight-scanner/static/css/main.fa987eac.chunk.css"
+  },
+  {
+    "revision": "32b5f3ff1e31b5b6e28c",
+    "url": "/warsawjs-workshop-20-flight-scanner/static/js/main.32b5f3ff.chunk.js"
+  },
+  {
+    "revision": "f396933118368548b2e9",
+    "url": "/warsawjs-workshop-20-flight-scanner/static/js/runtime~main.f3969331.js"
+  },
+  {
+    "revision": "29afbd18dae83a118ed3",
+    "url": "/warsawjs-workshop-20-flight-scanner/static/js/2.29afbd18.chunk.js"
+  },
+  {
+    "revision": "406f1a35a5467233a25f4ea51cba2d54",
+    "url": "/warsawjs-workshop-20-flight-scanner/static/media/plane.406f1a35.svg"
+  },
+  {
+    "revision": "979d7d9fbc85710142565d4fe0a244e3",
+    "url": "/warsawjs-workshop-20-flight-scanner/static/media/arrow.979d7d9f.svg"
+  },
+  {
+    "revision": "a6ceb5a6d76016fdbdb0a3bbb63e2a6f",
+    "url": "/warsawjs-workshop-20-flight-scanner/index.html"
+  }
+];
